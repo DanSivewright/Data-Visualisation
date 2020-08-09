3b50676e5331c027d70d6f0ccbb8698014ecdc9b
DanSivewright <dan@wixels.com>
2020-08-09 08:04:49 +0200

    Create LineChart.js

---
+++ src/components/line/LineChart.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { select, line, axisBottom, scaleLinear } from 'd3'
+
+export const LineChart = () => {
+  const [data, setData] = React.useState([10, 50, 30, 40, 20, 70, 50])
+  const svgRef = React.useRef()
+
+  React.useEffect(() => {
+    const svg = select(svgRef.current)
+
+    // Axis setup
+    const xScale = scaleLinear().domain([0, data.length - 1]).range([0, 600])
+    const yScale = scaleLinear().domain([0, 80]).range([150, 0])
+    const xAxis = axisBottom(xScale)
+
+    svg
+      .select('.x-axis')
+      .style('transform', 'translateY(149px)')
+      .call(xAxis)
+
+    // Line graph setup
+    const lineGraph = line()
+      .x((value, index) => xScale(index))
+      .y(yScale)
+
+    svg
+      .selectAll('.line').data([data])
+      .join('path')
+      .attr('class', 'line')
+      .attr('d', value => lineGraph(value))
+      .attr('fill', 'none')
+      .attr('stroke', 'red')
+  }, [data])
+  return (
+    <div>
+      <h2>Line Chart Testing</h2>
+      <svg ref={svgRef}>
+        <g className='x-axis' />
+      </svg>
+    </div>
+  )
+}
